4918a93a915f484a07013df97d8916ea006f5a0e
FBruynbroeck <francois@affinitic.be>
2014-11-27 16:58:08 +0100

    Add new upgradeStep: Clean old GenericSetup Step

---
+++ cpskin/migration/upgradesteps.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+def cleanupRegistry(context):
+    OLD_STEPS = ['acptheme.cpskin3.uninstall',
+                 'acptheme.cpskin3.extra',
+                 'acptheme.cpskin3.various',
+                 'cpskin.migration.after',
+                 'directory-Update-RoleMappings',
+                 'directory-postInstall',
+                 'directory-Hide-Tools-From-Navigation',
+                 'remember-uninstall']
+    registry = context.getImportStepRegistry()
+    for old_step in OLD_STEPS:
+        if old_step in registry.listSteps():
+            registry.unregisterStep(old_step)
+            # Unfortunately we manually have to signal the context
+            # (portal_setup) that it has changed otherwise this change is
+            # not persisted.
+            context._p_changed = True
